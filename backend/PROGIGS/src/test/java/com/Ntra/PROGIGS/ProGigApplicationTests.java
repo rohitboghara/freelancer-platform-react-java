588b0f1a45840a463d4d85ed1aad2cbac4e718a0
rootadmin12321 <rp.rohit0099@gmail.com>
2025-09-27 12:06:58 +0530

    change test file

--- backend/PROGIGS/src/test/java/com/Ntra/PROGIGS/ProGigApplicationTests.java
+++ backend/PROGIGS/src/test/java/com/Ntra/PROGIGS/ProGigApplicationTests.java
@@ -1,13 +1,14 @@
-package com.Ntra.PROGIGS;
+package com.ntra.progigs;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProgigsApplicationTests {
+class ProGigApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
+
